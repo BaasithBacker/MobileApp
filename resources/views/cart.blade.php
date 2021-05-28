8b97b07f08b6f610c57a4573383e5443f59aa49a
BaasithBacker <bh.basithbacker@gmail.com>
2021-05-28 19:43:22 +0530

    updated

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -4,10 +4,7 @@
 @section('content')
 
 <body>
-  <?php
-use App\Http\Controllers\item;
-$tp=item::totalprice();
-?>
+  
   <header>
     <!-- Header Start -->
 
@@ -105,7 +102,7 @@ $tp=item::totalprice();
                       <h5>Subtotal</h5>
                     </td>
                     <td>
-                      <h5>Rs.{{ $tp }}</h5>
+                      {{-- <h5>Rs.{{ $tp }}</h5> --}}
                     </td>
                   </tr>
                   <tr class="shipping_area">
